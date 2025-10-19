9283b38d7a1fe3ebecd4fad964b1f49a415fd4fb
Aastha Todewar <aasthatodewar@gmail.com>
2025-10-19 09:52:07 +0530

    Add files via upload

---
+++ Fields.java
@@ -0,0 +1,10 @@
+package Interfaces;
+
+public interface Fields {
+    public void PersonalDetail();
+    public void SeatSelection();
+    public void DisplayTempselection();
+    public void paymentmethod();
+
+
+}
